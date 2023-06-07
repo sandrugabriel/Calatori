517e0c9fe044bce2a23915cfac9135cb508bab9f
Sandru Gabriel <gabi200611@yahoo.com>
2023-06-08 02:21:26 +0300

    done

---
+++ Calatori/Models/Croaziere.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Calatori.Models
+{
+    internal class Croaziere
+    {
+
+        private int id;
+        private int tipCroaziera;
+        private List<int> listPorturi;
+        private DateTime dataStart;
+        private DateTime dataEnd;
+        private int pret;
+        private int nrPasageri;
+
+        public Croaziere(int id, int tipCroaziera, List<int> listPorturi, DateTime dataStart, DateTime dataEnd, int pret, int nrPasageri)
+        {
+            this.id = id;
+            this.tipCroaziera = tipCroaziera;
+            this.listPorturi = listPorturi;
+            this.dataStart = dataStart;
+            this.dataEnd = dataEnd;
+            this.pret = pret;
+            this.nrPasageri = nrPasageri;
+        
+        }
+
+        public Croaziere(string text) {
+
+            string[] prop = text.Split(',');
+            this.id = int.Parse(prop[0]);
+            this.tipCroaziera = int.Parse(prop[1]);
+            int ct = 2;
+            for(int i=0;i<listPorturi.Count;i++)
+            {
+                this.listPorturi[i] = int.Parse(prop[ct]);
+                ct++;
+            }
+        
+            this.dataStart = DateTime.Parse(prop[ct]);
+            this.dataEnd= DateTime.Parse(prop[ct+1]);
+            this.pret = int.Parse(prop[ct + 2]);
+            this.nrPasageri = int.Parse((prop[ct + 3]));
+
+        }
+
+        public int getId() { return id; }
+
+        public int getTipCroaziera()
+        {
+            return tipCroaziera;
+        }
+
+        public List<int> getListPorturi()
+        {
+            return listPorturi;
+        }
+
+        public DateTime getDataStart()
+        {
+            return dataStart;
+        }
+
+        public DateTime getDataEnd()
+        {
+            return dataEnd;
+        }
+
+        public int getNumPasageri() { 
+        
+            return nrPasageri;
+        }
+
+        public int getPret()
+        {
+            return pret;
+        }
+
+    }
+}
